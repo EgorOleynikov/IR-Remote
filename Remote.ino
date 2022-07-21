6e4cee63668135bca4fd12f0d850be78348d185d
EgorOleynikov <reichweitedeswahnsinns@gmail.com>
2022-07-22 00:06:48 +0300

    commit

---
+++ Remote.ino
@@ -0,0 +1,45 @@
+/* rawR&cv.ino Example sketch for IRLib2
+ *  Illustrate how to capture raw timing values for an unknow protocol.
+ *  You will capture a signal using this sketch. It will output data the 
+ *  serial monitor that you can cut and paste into the "rawSend.ino"
+ *  sketch.
+ */
+// Recommend only use IRLibRecvPCI or IRLibRecvLoop for best results
+// #include "IRLibAll.h"
+#include <IRLibRecvPCI.h>
+#include <IRLibDecodeBase.h>
+#include <IRLib_P01_NEC.h>
+#include <IRLibCombo.h>
+
+IRrecvPCI myReceiver(3);//pin number for the receiver
+IRdecode myDecoder;   //create decoder
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Ready"));
+}
+
+void loop() {
+  if (Serial.available() > 0){
+    int incomingByte = Serial.read();
+    if (incomingByte == '1'){
+      myReceiver.enableIRIn(); // Start the receiver
+    } else if (incomingByte == '0') {
+      myReceiver.disableIRIn();
+    }
+  }
+  //Continue looping until you get a complete signal received
+  if (myReceiver.getResults()) {
+    myDecoder.decode();           //Decode it
+    myDecoder.dumpResults(false);  //Now print results. Use false for less detail
+    Serial.print("uint16_t rawData[");
+    Serial.print(recvGlobal.recvLength,DEC);
+    Serial.print("]={");
+    for(bufIndex_t i=1;i<recvGlobal.recvLength;i++) {
+      Serial.print(recvGlobal.recvBuffer[i],DEC);
+      Serial.print(F(", "));
+    }
+    Serial.println(F("1000};"));//Add arbitrary trailing space
+    myReceiver.enableIRIn();      //Restart receiver
+  }
+}
--- Remote.py
+++ Remote.py
@@ -1,6 +1,95 @@
-from curses import baudrate
+from curses import baudrate, echo, window
+from ipaddress import collapse_addresses
 import serial
-ser = serial.Serial(port="COM5", baudrate=9600)  # open serial port
-print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
-ser.close()             # close port
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+ARDUINO_CONF = {"port": "COM5", "baudrate":"9600"};
+
+# UI section
+root = Tk()
+root.title("Remote")
+
+content = ttk.Frame(root)
+frame = ttk.Notebook(content, width=700, height=600)
+page1 = ttk.Frame(frame)   # first page, which would get widgets gridded into it
+page2 = ttk.Frame(frame)   # second page
+frame.add(page1, text='One')
+frame.add(page2, text='Two')
+
+#vars for buttons
+onevar = BooleanVar(value=True)
+twovar = BooleanVar(value=False)
+threevar = BooleanVar(value=True)
+#
+
+#funcs
+def popUp_waitForReadings():
+    def dismiss():
+        with serial.Serial(port=ARDUINO_CONF["port"], baudrate=ARDUINO_CONF["baudrate"]) as ser:
+            ser = serial.Serial()  # open serial port
+            ser.write(bytearray(1)) # tells arduino to start listening
+            line = ser.readline()
+            print(line)
+            ser.write(bytearray(0))
+            ser.close()
+
+        dlg.grab_release()
+        dlg.destroy()
+
+    dlg = Toplevel(root)
+    ttk.Button(dlg, text="Done", command=dismiss).grid()
+    dlg.protocol("WM_DELETE_WINDOW", dismiss) # intercept close button
+    dlg.transient(root)   # dialog window is related to main
+    dlg.wait_visibility() # can't grab until window appears, so we wait
+    dlg.grab_set()        # ensure all input goes to our window
+    dlg.wait_window()     # block until window is destroyed
+
+#
+
+#define members
+#p1
+one = ttk.Checkbutton(page1, text="One", variable=onevar, onvalue=True)
+two = ttk.Checkbutton(page1, text="Two", variable=twovar, onvalue=True)
+three = ttk.Checkbutton(page1, text="Three", variable=threevar, onvalue=True)
+#
+#p2
+dataName = ttk.Entry(page2)
+add = ttk.Button(page2, text="Add", command=popUp_waitForReadings)
+#
+#
+
+#initial
+content.grid(column=0, row=0)
+frame.grid(column=0, row=0, columnspan=3, rowspan=2)
+#
+#p1 layout
+one.grid(column=0, row=1)
+two.grid(column=0, row=2)
+three.grid(column=0, row=3)
+#
+#p2 layout
+dataName.grid(column=0, row=0)
+add.grid(column=1, row=0)
+#
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+content.columnconfigure(0, weight=3)
+content.columnconfigure(1, weight=3)
+content.columnconfigure(2, weight=3)
+content.columnconfigure(3, weight=1)
+content.columnconfigure(4, weight=1)
+content.rowconfigure(1, weight=1)
+
+root.mainloop()
+#
+
+# arduino section
+# ser = serial.Serial(port="COM5", baudrate=9600)  # open serial port
+# print(ser.name)         # check which port was really used
+# res = ser.read_all()
+# ser.close()
+# print(res)
+#
